1629e64194b05bf5a0be07c41f81cec7111ce4df
MLCVL0675 <mattalaxmichandravyas@gmail.com>
2025-09-10 20:46:55 +0530

    NEVER 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/messageRoutes.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
