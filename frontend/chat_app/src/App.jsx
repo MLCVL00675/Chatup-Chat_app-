162a3571549d4d07dbcb04f7b6dd73859989f479
MLCVL0675 <mattalaxmichandravyas@gmail.com>
2025-09-10 18:15:39 +0530

    Hopefully final commit

--- frontend/chat_app/src/App.jsx
+++ frontend/chat_app/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar  from "./components/navbar";
+import Navbar  from "./components/Navbar.jsx";
 import { Route, Routes,Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
